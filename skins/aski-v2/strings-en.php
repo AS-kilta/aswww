33ba4d4d3ece6e9c072ffc8bfd1925d36dcc6b96
www-data <www-data@0adfd92d-e4da-4def-8250-4749ea1df07c>
2009-03-04 11:54:27 +0000

    Footerin vuosi vaihdettu.
    git-svn-id: file:///home/severi/AS/as.ayy.fi/svn-to-git/repositories/aswww@103 0adfd92d-e4da-4def-8250-4749ea1df07c

--- skins/aski-v2/strings-en.php
+++ skins/aski-v2/strings-en.php
@@ -1,7 +1,7 @@
 <?php
 
 $_['logoUrl'] = '/skins/aski-v2/logo-en.png';
-$_['footerText'] = '&copy; Guild of Automation and Systems Technology, 2008';
+$_['footerText'] = '&copy; Guild of Automation and Systems Technology, 2009';
 $_['title'] = 'Guild of Automation and Systems Technology';
 $_['Exam_archive'] = 'Exam archive';
 $_['Gallery'] = 'Gallery';
--- skins/aski-v2/strings-fi.php
+++ skins/aski-v2/strings-fi.php
@@ -1,7 +1,7 @@
 <?php
 
 $_['logoUrl'] = '/skins/aski-v2/logo.png';
-$_['footerText'] = '&copy; Automaatio- ja systeemitekniikan kilta, 2008';
+$_['footerText'] = '&copy; Automaatio- ja systeemitekniikan kilta, 2009';
 $_['title'] = 'Automaatio- ja systeemitekniikan kilta';
 $_['Exam_archive'] = 'Tenttiarkisto';
 $_['Gallery'] = 'Galleria';
